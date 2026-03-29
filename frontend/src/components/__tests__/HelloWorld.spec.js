e70df01e5bef3ff2133febc2338c5825197b9d26
AJMM11 <ajmartinmanzano@gmail.com>
2026-03-29 16:58:02 +0200

    added sample test to check if pipeline works

--- frontend/src/components/__tests__/HelloWorld.spec.js
+++ frontend/src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,7 @@
 import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
-
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+describe('Placeholder', () => {
+  it('siempre pasa', () => {
+    expect(true).toBe(true)
   })
 })
