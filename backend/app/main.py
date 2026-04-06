b75a33e840edd00a094654976c5e9fd452b4affd
AJMM11 <ajmartinmanzano@gmail.com>
2026-04-06 02:26:13 +0200

    removed cors from fastapi to let nginx manage it

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,13 +13,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Pedalboard IR API", lifespan=lifespan)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173",os.getenv("FRONTEND_URL")],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173",os.getenv("FRONTEND_URL")],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(irs.router,  prefix="/irs",  tags=["irs"])
